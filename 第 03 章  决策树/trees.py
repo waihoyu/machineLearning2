3241b1a7db6dedbd16f28422f658840b0ea8652c
waihoyu <13701343809@qq.com>
2019-04-25 23:42:30 +0800

    update

---
+++ 第 03 章  决策树/trees.py
@@ -0,0 +1,10 @@
+
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            reducedFeatVec.append(reducedFeatVec)
+    return  retDataSet
